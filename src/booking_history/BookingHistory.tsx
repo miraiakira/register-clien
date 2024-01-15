383b1b9d9d9f4bfff6b1e8351e2be978e8b6c615
xdong <hy44jt@gmail.com>
2024-01-15 14:37:46 +0900

    booking history

--- src/booking_history/BookingHistory.tsx
+++ src/booking_history/BookingHistory.tsx
@@ -1,3 +1,75 @@
+import { Button, DatePicker, Form, Input, TimePicker } from "antd";
+import { useEffect } from "react";
+import { useForm } from "antd/es/form/Form";
+import "./booking_history.css";
+import { SearchBooking } from "../interface/interfaces";
+
+function getUserInfo() {
+	const userInfoStr = localStorage.getItem("user_info");
+
+	if (userInfoStr) {
+		return JSON.parse(userInfoStr);
+	}
+}
+
 export function BookingHistory() {
-  return <div>BookingHistory</div>;
+	const searchBooking = async (values: SearchBooking) => {};
+
+	const [form] = useForm();
+
+	useEffect(() => {
+		searchBooking({
+			username: getUserInfo().username,
+			meetingRoomName: form.getFieldValue("meetingRoomName"),
+			meetingRoomPosition: form.getFieldValue("meetingRoomPosition"),
+			rangeStartDate: form.getFieldValue("rangeStartDate"),
+			rangeStartTime: form.getFieldValue("rangeStartTime"),
+			rangeEndDate: form.getFieldValue("rangeEndDate"),
+			rangeEndTime: form.getFieldValue("rangeEndTime"),
+		});
+	}, []);
+
+	return (
+		<div id="bookingHistory-container">
+			<div className="bookingHistory-form">
+				<Form
+					form={form}
+					onFinish={searchBooking}
+					name="search"
+					layout="inline"
+					colon={false}
+				>
+					<Form.Item label="会议室名称" name="meetingRoomName">
+						<Input />
+					</Form.Item>
+
+					<Form.Item label="预定开始日期" name="rangeStartDate">
+						<DatePicker />
+					</Form.Item>
+
+					<Form.Item label="预定开始时间" name="rangeStartTime">
+						<TimePicker />
+					</Form.Item>
+
+					<Form.Item label="预定结束日期" name="rangeEndDate">
+						<DatePicker />
+					</Form.Item>
+
+					<Form.Item label="预定结束时间" name="rangeEndTime">
+						<TimePicker />
+					</Form.Item>
+
+					<Form.Item label="位置" name="meetingRoomPosition">
+						<Input />
+					</Form.Item>
+
+					<Form.Item label=" ">
+						<Button type="primary" htmlType="submit">
+							搜索预定历史
+						</Button>
+					</Form.Item>
+				</Form>
+			</div>
+		</div>
+	);
 }
--- src/interface/interfaces.ts
+++ src/interface/interfaces.ts
@@ -1,136 +1,190 @@
-import { message } from 'antd';
-import axios from 'axios';
+import { message } from "antd";
+import axios from "axios";
+import dayjs from "dayjs";
 
 interface RegisterUser {
-  username: string;
-  nickName: string;
-  password: string;
-  email: string;
-  captcha: string;
+	username: string;
+	nickName: string;
+	password: string;
+	email: string;
+	captcha: string;
 }
 
 export interface UpdatePassword {
-  email: string;
-  username: string;
-  captcha: string;
-  password: string;
-  confirmPassword: string;
+	email: string;
+	username: string;
+	captcha: string;
+	password: string;
+	confirmPassword: string;
 }
 
 interface UserInfo {
-  headPic: string;
-  nickName: string;
-  email: string;
-  captcha: string;
+	headPic: string;
+	nickName: string;
+	email: string;
+	captcha: string;
 }
 
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:5500/',
-  timeout: 3000,
+	baseURL: "http://localhost:5500/",
+	timeout: 3000,
 });
 
 axiosInstance.interceptors.request.use(function (config) {
-  const accessToken = localStorage.getItem('access_token');
+	const accessToken = localStorage.getItem("access_token");
 
-  if (accessToken) {
-    config.headers.authorization = 'Bearer ' + accessToken;
-  }
-  return config;
+	if (accessToken) {
+		config.headers.authorization = "Bearer " + accessToken;
+	}
+	return config;
 });
 
 async function refreshToken() {
-  const res = await axios.get('user/refresh', {
-    params: {
-      refresh_token: localStorage.getItem('refresh_token'),
-    },
-  });
-  localStorage.setItem('access_token', res.data.access_token);
-  localStorage.setItem('refresh_token', res.data.refresh_token);
-  return res;
+	const res = await axios.get("user/refresh", {
+		params: {
+			refresh_token: localStorage.getItem("refresh_token"),
+		},
+	});
+	localStorage.setItem("access_token", res.data.access_token);
+	localStorage.setItem("refresh_token", res.data.refresh_token);
+	return res;
 }
 
 axiosInstance.interceptors.response.use(
-  (response) => {
-    return response;
-  },
-  async (error) => {
-    let { data, config } = error.response;
+	(response) => {
+		return response;
+	},
+	async (error) => {
+		let { data, config } = error.response;
 
-    if (data.code === 401 && !config.url.includes('/user/refresh')) {
-      const res = await refreshToken();
+		if (data.code === 401 && !config.url.includes("/user/refresh")) {
+			const res = await refreshToken();
 
-      if (res.status === 200) {
-        return axios(config);
-      } else {
-        message.error(res.data);
+			if (res.status === 200) {
+				return axios(config);
+			} else {
+				message.error(res.data);
 
-        setTimeout(() => {
-          window.location.href = '/login';
-        }, 1500);
-      }
-    } else {
-      return error.response;
-    }
-  }
+				setTimeout(() => {
+					window.location.href = "/login";
+				}, 1500);
+			}
+		} else {
+			return error.response;
+		}
+	}
 );
 
 export async function login(username: string, password: string) {
-  return await axiosInstance.post('/user/login', {
-    username,
-    password,
-  });
+	return await axiosInstance.post("/user/login", {
+		username,
+		password,
+	});
 }
 
 export async function registerCaptcha(email: string) {
-  return await axiosInstance.get('/user/register-captcha', {
-    params: {
-      address: email,
-    },
-  });
+	return await axiosInstance.get("/user/register-captcha", {
+		params: {
+			address: email,
+		},
+	});
 }
 
 export async function register(registerUser: RegisterUser) {
-  return await axiosInstance.post('/user/register', registerUser);
+	return await axiosInstance.post("/user/register", registerUser);
 }
 
 export async function updatePasswordCaptcha(email: string) {
-  return await axiosInstance.get('/user/update_password/captcha', {
-    params: {
-      address: email,
-    },
-  });
+	return await axiosInstance.get("/user/update_password/captcha", {
+		params: {
+			address: email,
+		},
+	});
 }
 
 export async function updatePassword(data: UpdatePassword) {
-  return await axiosInstance.post('/user/update_password', data);
+	return await axiosInstance.post("/user/update_password", data);
 }
 
 export async function getUserInfo() {
-  return await axiosInstance.get('/user/info');
+	return await axiosInstance.get("/user/info");
 }
 
 export async function updateInfo(data: UserInfo) {
-  return await axiosInstance.post('/user/update', data);
+	return await axiosInstance.post("/user/update", data);
 }
 
 export async function updateUserInfoCaptcha() {
-  return await axiosInstance.get('/user/update/captcha');
+	return await axiosInstance.get("/user/update/captcha");
 }
 
 export async function searchMeetingRoomList(
-  name: string,
-  capacity: number,
-  equipment: string,
-  pageNo: number,
-  pageSize: number
+	name: string,
+	capacity: number,
+	equipment: string,
+	pageNo: number,
+	pageSize: number
 ) {
-  return await axiosInstance.get('/meeting-room/list', {
-    params: {
-      name,
-      capacity,
-      equipment,
-      pageNo,
-      pageSize,
-    },
-  });
+	return await axiosInstance.get("/meeting-room/list", {
+		params: {
+			name,
+			capacity,
+			equipment,
+			pageNo,
+			pageSize,
+		},
+	});
+}
+
+export interface SearchBooking {
+	username: string;
+	meetingRoomName: string;
+	meetingRoomPosition: string;
+	rangeStartDate: Date;
+	rangeStartTime: Date;
+	rangeEndDate: Date;
+	rangeEndTime: Date;
+}
+
+export async function bookingList(
+	searchBooking: SearchBooking,
+	pageNo: number,
+	pageSize: number
+) {
+	let bookingTimeRangeStart;
+	let bookingTimeRangeEnd;
+
+	if (searchBooking.rangeStartDate && searchBooking.rangeStartTime) {
+		const rangeStartDateStr = dayjs(searchBooking.rangeStartDate).format(
+			"YYYY-MM-DD"
+		);
+		const rangeStartTimeStr = dayjs(searchBooking.rangeStartTime).format(
+			"HH:mm"
+		);
+		bookingTimeRangeStart = dayjs(
+			rangeStartDateStr + " " + rangeStartTimeStr
+		).valueOf();
+	}
+
+	if (searchBooking.rangeEndDate && searchBooking.rangeEndTime) {
+		const rangeEndDateStr = dayjs(searchBooking.rangeEndDate).format(
+			"YYYY-MM-DD"
+		);
+		const rangeEndTimeStr = dayjs(searchBooking.rangeEndTime).format("HH:mm");
+		bookingTimeRangeEnd = dayjs(
+			rangeEndDateStr + " " + rangeEndTimeStr
+		).valueOf();
+	}
+
+	return await axiosInstance.get("/booking/list", {
+		params: {
+			username: searchBooking.username,
+			meetingRoomName: searchBooking.meetingRoomName,
+			meetingRoomPosition: searchBooking.meetingRoomPosition,
+			bookingTimeRangeStart,
+			bookingTimeRangeEnd,
+			pageNo: pageNo,
+			pageSize: pageSize,
+		},
+	});
 }
